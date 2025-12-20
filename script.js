84754056c4fe697bb94c1995df6402614491cda1
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2025-12-20 23:15:26 +0530

    populate book details from myLibrary to the DOM

--- script.js
+++ script.js
@@ -16,3 +16,8 @@ addBookToLibrary("2", "Lotpot", "Diamond Comics");
 
 let newLib = myLibrary.map(objInFocus=> objInFocus);
 
+let bookName = document.querySelector("#bookName");
+let writerName = document.querySelector("#writerName");
+
+bookName.textContent = myLibrary[0].name;
+writerName.textContent = myLibrary[0].writer;
