5b6c1bda8e38c74c09db9b0cf28fe58256e2cbfa
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2026-01-16 14:54:37 +0530

    add comment to improve readability

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ function addBookToLibrary (id, name, writer) {
     myLibrary.push(bookObject); //bookObject is pushed into array myLibrary
 }   
 
-addBookToLibrary("1", "Chacha Chaudhary", "Diamond Comics"); //passing values to function
-addBookToLibrary("2", "Lotpot", "Diamond Comics"); //passing values to function
+addBookToLibrary("1", "Chacha Chaudhary", "Chacha Comics"); //passing values to function
+addBookToLibrary("2", "Lotpot", "Lotpot Comics"); //passing values to function
 
 let bookName = document.querySelector("#bookName");
 let writerName = document.querySelector("#writerName");
 
-bookName.textContent = myLibrary[0].name;
-writerName.textContent = myLibrary[0].writer;
+bookName.textContent = myLibrary[0].name;//takes out name the first element of the myLibrary array
+writerName.textContent = myLibrary[0].writer;//takes out writer name of the first element of the myLibrary array
