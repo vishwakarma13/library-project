132af9863dabdc449355fe30342c29fc1a526e8f
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2026-01-20 02:04:32 +0530

    add bookname1 to table cell

--- script.js
+++ script.js
@@ -21,9 +21,12 @@ function displaysEachBook () {
     }
 };
 
-//to add the contents of book in table cells
+//to add the contents of book1 in table cells
 let bookname1 = document.querySelector("#bookname1");
 let writername1 = document.querySelector("#writername1");
 let pagesin1 = document.querySelector("#pagesin1");
+
+bookname1.textContent = myLibrary[0].name; //overrides the existing text in the table cell
+
 
 displaysEachBook ();//calls the displaysEachBook function
