05f47a8ae8ccae0d95729bf5975020e4967a523c
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2026-01-20 02:36:01 +0530

    add content to book id cell

--- script.js
+++ script.js
@@ -22,17 +22,22 @@ function displaysEachBook () {
 };
 
 //to add the contents of book1 in table cells
+let bookid1 = document.querySelector("#bookid1");
 let bookname1 = document.querySelector("#bookname1");
 let writername1 = document.querySelector("#writername1");
 let pagesin1 = document.querySelector("#pagesin1");
 
+bookid1.textContent = myLibrary[0].id;
 bookname1.textContent = myLibrary[0].name; //overrides the existing text in the table cell
 writername1.textContent = myLibrary[0].writer; //overrides the existing text in the table cell
 
+
+let bookid2 = document.querySelector("#bookid2");
 let bookname2 = document.querySelector("#bookname2");
 let writername2 = document.querySelector("#writername2");
 let pagesin2 = document.querySelector("#pagesin2");
 
+bookid2.textContent = myLibrary[1].id;
 bookname2.textContent = myLibrary[1].name; //overrides the existing text in the table cell
 writername2.textContent = myLibrary[1].writer; //overrides the existing text in the table cell
 
