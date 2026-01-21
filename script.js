1e8b3119278e58ddc7fdff93c1476d0ce1b179af
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2026-01-21 23:49:20 +0530

    remove manual ids from function call

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function addBookToLibrary (name, writer) {
     myLibrary.push(bookObject); //bookObject is pushed into array myLibrary
 }   
 
-addBookToLibrary("1", "Chacha Chaudhary", "Chacha Comics"); //manually passing values to function
-addBookToLibrary("2", "Lotpot", "Lotpot Comics"); //manually passing values to function
+addBookToLibrary("Chacha Chaudhary", "Chacha Comics"); //manually passing values to function
+addBookToLibrary("Lotpot", "Lotpot Comics"); //manually passing values to function
 
 //function that loops through the array myLibrary (which stores all the created books)
 function displaysEachBook () {
